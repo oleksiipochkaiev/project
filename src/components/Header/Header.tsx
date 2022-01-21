bd32f5cad5db3680b3d8df3042f45e0d8af4028c
Oleksii Pochkaiev <oleksiypochkayev@gmail.com>
2022-01-21 20:43:59 +0200

    .

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
   return (
     <header className="header ">
       <div className="header__container">
-        <Link to="/project">
+        <Link to="/posts">
           <Logo />
         </Link>
       </div>
